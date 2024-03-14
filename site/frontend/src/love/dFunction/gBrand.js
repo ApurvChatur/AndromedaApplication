db2f95868c256cd4bb09c3604eccb6f45cfe317e
ApurvChatur <chaturapurv@gmail.com>
2024-03-15 03:13:25 +0530

    New Mess

--- site/frontend/src/love/dFunction/gBrand.js
+++ site/frontend/src/love/dFunction/gBrand.js
@@ -19,8 +19,8 @@ const brand = () => {
 	var brandObject
 
 		brandObject = {
-			logo: beehive,
-			name: "Beehive Corporation",
+			logo: andromeda,
+			name: "Andromeda Organisation",
 			app: "https://beehive-frontend.netlify.app/",
 			admin: "https://beehive-admin.netlify.app/"
 		}
